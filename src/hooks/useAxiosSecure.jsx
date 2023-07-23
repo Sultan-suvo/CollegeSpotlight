ace8b8a2af5fce7cd48029d31c2152cdc3ee4b66
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-07-23 20:02:43 +0600

    replace vercel

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProvider';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'https://collegic-server.vercel.app',
 });
 
 const useAxiosSecure = () => {
